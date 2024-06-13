527ed540c414b7147e373088d858595a4f4f15df
Mark Plewis <markplewis1@gmail.com>
2024-06-12 23:36:07 -0400

    Completes nearly all of the Brackeys tutorial

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var last_direction = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,15 +15,36 @@ func _physics_process(delta: float) -> void:
     velocity.y += gravity * delta
 
   # Handle jump.
-  if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+  if Input.is_action_just_pressed("jump") and is_on_floor():
     velocity.y = JUMP_VELOCITY
 
-  # Get the input direction and handle the movement/deceleration.
-  # As good practice, you should replace UI actions with custom gameplay actions.
-  var direction := Input.get_axis("ui_left", "ui_right")
+  # Get the input direction: -1, 0, 1
+  var direction := Input.get_axis("move_left", "move_right")
+
+  # Handle the movement/deceleration
   if direction:
     velocity.x = direction * SPEED
   else:
     velocity.x = move_toward(velocity.x, 0, SPEED)
+
+  # Flip the sprite
+  if direction > 0:
+    animated_sprite.flip_h = false
+  elif direction < 0:
+    animated_sprite.flip_h = true
+  else:
+    animated_sprite.flip_h = last_direction < 0
+
+  if direction != 0:
+    last_direction = direction
+
+  # Play animations
+  if is_on_floor():
+    if direction == 0:
+      animated_sprite.play("idle")
+    else:
+      animated_sprite.play("run")
+  else:
+    animated_sprite.play("jump")
 
   move_and_slide()
