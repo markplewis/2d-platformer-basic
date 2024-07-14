5e3478cfffe1f135352567945e403b2cab199792
Mark Plewis <markplewis1@gmail.com>
2024-07-14 11:14:58 -0400

    Starts building main menu UI

---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,30 @@
+class_name MainMenu extends Control
+
+signal start_game
+
+@onready var buttons_v_box: VBoxContainer = %ButtonsVBox
+
+
+func _ready() -> void:
+  _focus_button()
+
+
+func _on_start_button_pressed() -> void:
+  start_game.emit()
+  hide()
+
+
+func _on_quit_button_pressed() -> void:
+  get_tree().quit()
+
+
+func _on_visibility_changed() -> void:
+  if visible:
+    _focus_button()
+
+
+func _focus_button() -> void:
+  if buttons_v_box:
+    var button: Button = buttons_v_box.get_child(0)
+    if button is Button:
+      button.grab_focus()
