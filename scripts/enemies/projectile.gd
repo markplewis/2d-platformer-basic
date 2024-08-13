81745d26de6aba3b976123dc5dfc9411cc66a702
Mark Plewis <markplewis1@gmail.com>
2024-08-13 00:03:38 -0400

    Adds projectile destruction animation

--- scripts/enemies/projectile.gd
+++ scripts/enemies/projectile.gd
@@ -1,8 +1,12 @@
 class_name Projectile extends Area2D
 
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
 var damage: int = 10
 var direction: Vector2 = Vector2.ZERO
 var speed: float = 80.0
+
+var _destroyed: bool = false
 
 
 func _ready() -> void:
@@ -10,19 +14,25 @@ func _ready() -> void:
 
 
 func _physics_process(delta) -> void:
-  position.x += direction.x * speed * delta
+  if not _destroyed:
+    position.x += direction.x * speed * delta
+
+
+func _destroy() -> void:
+  _destroyed = true
+  _animation_player.play("destroy") # Calls queue_free() when animation ends
 
 
 func _on_body_entered(body: Node2D) -> void:
   if body is TileMap:
-    queue_free()
+    _destroy()
 
 
 func _on_area_entered(area: Area2D) -> void:
   if area.owner.has_method("take_damage"):
     area.owner.take_damage(self, damage)
-    queue_free()
+    _destroy()
 
 
 func _on_timer_timeout() -> void:
-  queue_free()
+  _destroy()
